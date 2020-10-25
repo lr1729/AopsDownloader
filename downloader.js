373be59bca4e913ce80029010ddafd471b3960a0
ling1729 <renlingfeng21@gmail.com>
2020-10-25 09:25:52 -0700

    Add login check and fix bug

--- downloader.js
+++ downloader.js
@@ -10,34 +10,53 @@ const puppeteer = require('puppeteer-core');
   const tmp = os.tmpdir();
 
   const exec = await download({
-      revision: 694644,
-      installPath: `${tmp}/.local-chromium`})
+    revision: 694644,
+    installPath: `${tmp}/.local-chromium`})
 
   const browser = await puppeteer.launch({
-      executablePath: exec
+    executablePath: exec,
+    headless: false
   });
 
   const page = await browser.newPage();
 
+  // Ask questions
   var classURL = await askQuestion("Paste the url of your class (e.g. https://artofproblemsolving.com/class/2156-calculus) ");
   if(classURL.slice(-1) == "/")
     classURL = classURL.slice(0, -1);
   await page.goto(classURL, {waitUntil: 'networkidle2'});
+  console.log(await page.evaluate('document.querySelector(".error").getAttribute("style") === null'));
   var saveTranscripts = await askQuestion("Save transcripts? (yes/no) ");
   if(saveTranscripts == "yes")
   var transcriptURL = await askQuestion("Paste the url of the first week's transcript ");
-  if(transcriptURL.slice(-1) == "/")
-    transcriptURL = transcriptURL.slice(0, -1);
   var saveHomework = await askQuestion("Save homework? (yes/no) ");
   var weeks = await askQuestion("How many weeks to save? ");
+
+  // Login
   var username = await askQuestion("Please enter your username ");
-  var password = await askQuestion("Please enter your password ");
-  var transcriptNum = parseInt(transcriptURL.split('/')[transcriptURL.split('/').length - 1]);
   await page.type('#login-username', username);
+  var password = await askQuestion("Please enter your password ");
   await page.type('#login-password', password);
   await page.click('#login-button');
-  await page.waitForNavigation({waitUntil: 'networkidle0'});
+  await sleep(1000);
+  while(await page.evaluate('document.querySelector(".error")') !== null && await page.evaluate('document.querySelector(".error").getAttribute("style") !== null')){
+    console.log("Login failed");
+    var username = await askQuestion("Please enter your username ");
+    await page.click('#login-username', {clickCount: 3})
+    await page.type('#login-username', username);
+    var password = await askQuestion("Please enter your password ");
+    await page.click('#login-password', {clickCount: 3})
+    await page.type('#login-password', password);
+    await page.click('#login-button');
+    await sleep(1000);
+  }
+  console.log("Logged in successfully");
+
+  // Save transcripts
   if(saveTranscripts == "yes"){
+    if(transcriptURL.slice(-1) == "/")
+      transcriptURL = transcriptURL.slice(0, -1);
+    var transcriptNum = parseInt(transcriptURL.split('/')[transcriptURL.split('/').length - 1]);
     console.log("Saving transcripts");
     for(let i = 1; i < parseInt(weeks) + 1; i++){
       let transcript = classURL + '/transcript/' + (transcriptNum + i - 1);
@@ -48,6 +67,7 @@ const puppeteer = require('puppeteer-core');
     }
   }
 
+  // Save homework
   if(saveHomework == "yes"){
     console.log("Saving homework");
     for(let i = 1; i < parseInt(weeks) + 1; i++){
@@ -72,4 +92,10 @@ function askQuestion(query) {
         rl.close();
         resolve(ans);
     }))
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
