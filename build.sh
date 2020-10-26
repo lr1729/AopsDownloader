07da6d0aae1bc744e73594302ddbaceff0a52a6d
ling1729 <renlingfeng21@gmail.com>
2020-10-25 21:07:37 -0700

    less verbose executables

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 mkdir -p ./build
 rm -f ./build/*
 echo "Building linux executable"
-pkg downloader.js -t node14-linux-x64 --output ./build/aopsdownloader-linux
+pkg downloader.js -t node14-linux-x64 --output ./build/aopsdl-linux
 echo "Building macos executable"
-pkg downloader.js -t node14-macos-x64 --output ./build/aopsdownloader-macos
+pkg downloader.js -t node14-macos-x64 --output ./build/aopsdl-macos
 echo "Building windows executable"
-pkg downloader.js -t node14-win-x64 --output ./build/aopsdownloader-windows
+pkg downloader.js -t node14-win-x64 --output ./build/aopsdl-windows
