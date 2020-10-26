5779d65543a0acb78ea01b6e8df6e9713b16bdad
ling1729 <renlingfeng21@gmail.com>
2020-10-25 20:55:25 -0700

    macos exists

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 mkdir -p ./build
 rm -f ./build/*
+echo "Building linux executable"
 pkg downloader.js -t node14-linux-x64 --output ./build/aopsdownloader-linux
-pkg downloader.js -t node14-win-x64 --output ./build/aopsdownloader-macos
+echo "Building macos executable"
+pkg downloader.js -t node14-macos-x64 --output ./build/aopsdownloader-macos
+echo "Building windows executable"
 pkg downloader.js -t node14-win-x64 --output ./build/aopsdownloader-windows
